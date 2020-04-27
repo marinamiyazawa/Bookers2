8bbfa3695da289bf3cbf60ff8b748743c8929c56
marinamiyazawa <mi.marina1030@gmail.com>
2020-04-27 19:16:45 +0900

    [Update]javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,24 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function(){
+      $('.jquery').on('click', function(){
+            $(this).css('color','red');
+      });
+});
+
+$(document).ready(function(){
+      $("#theTarget").skippr({
+            transition : 'slide',
+            speed : 1000,
+            easing : 'easeOutQuart',
+            navType : 'block',
+            childrenElementType : 'div',
+            arrows : true,
+            autoPlay : true,
+            autoPlayDuration : 1000,
+            keyboardOnAlways : true,
+            hidePrevious : false
+      });
+});
